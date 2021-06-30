6b46002e7eef791dcd3923259bb209a28a3959b4
runner38 <dhlrunner@gmail.com>
2021-07-01 02:53:24 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Doki Doki Literature Club Plus asset decrypter
+ Decrypt the encrypted asset file on the Doki Doki Literature Club Plus. (Files ending with the .cy extension)
---
+++ ddlcXordecrypter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ddlcXordecrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte key = 40;
+            int buffersize = 2048;
+            string filename = args[0];
+            Console.WriteLine("Infile: {0}", filename);
+            using(BinaryReader cy = new BinaryReader(File.OpenRead(filename)))
+            {
+                Console.WriteLine("Size: {0}", cy.BaseStream.Length);
+                using (BinaryWriter decrypted = new BinaryWriter(File.Create(filename + ".decrypted")))
+                {                   
+                    while(cy.BaseStream.Position < cy.BaseStream.Length)
+                    {
+                        byte[] buffer = cy.ReadBytes(buffersize);
+                        for(int i=0; i< buffer.Length; i++)
+                        {
+                            buffer[i] = (byte)(buffer[i] ^ key);
+                        }
+                        decrypted.Write(buffer);
+                    }
+                    Console.WriteLine("Decrypt Completed. out file: {0}.decrypted", filename);
+                }
+            }
+           
+        }
+    }
+}
